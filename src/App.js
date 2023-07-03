a6dd30a95ab5192acb5ab9ed282b2a1e0b25213f
CeDwukropek <emilkopytek@gmail.com>
2023-07-03 11:01:12 +0200

    works lol

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { Button } from './button';
 
-class App extends React.Component {
-  state = {
-    count: 0,
+function App() {
+  const [newItem, writeNewItem] = useState("")
+  const [items, setNewItem] = useState([]) 
+
+  function add() {
+    setNewItem(currentItems => {
+      return[
+        ...currentItems,
+        {id: crypto.randomUUID(), title: newItem}
+      ]
+    })
+
+    writeNewItem("")
   }
 
-  add = () => {
-    this.setState({count: this.state.count += 1})
-  }
-
-  minus = () => {
-    this.setState({count: this.state.count -= 1})
-  }
-  
-  render() {
-    
-      
-    return (
-      <>
-        <h1>counter: {this.state.count}</h1>
-        <Button text='Dodaj' onClick={this.add} />
-        <Button text='Odejmij' onClick={this.minus} />
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>Type here</h1>
+      <input
+        className='bg-slate-300'
+        value={newItem}
+        type='text'
+        onChange={e => writeNewItem(e.target.value)}
+      />
+      <button
+        className='px-6 py-4 bg-orange-300 ml-4'
+        onClick={() => add()}
+      >
+        add
+      </button>
+      {items.length === 0 && 'No items'}
+      {items.map(item => {
+        console.log('items :>> ', items);
+        console.log('item :>> ', item);
+        return(
+          <p key={item.id}>{item.title}</p>
+        )
+      })}
+    </>
+  );
 }
 
 export default App;
